e9632c7fe40a7c0e3a02123fbf8fcd6097e28282
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2026-03-09 10:37:46 +0100

    ci: fix error message assert on recent ES 8

--- tests/Elasticsearch/Query/Bulk/ElasticsearchBulkQueryTest.php
+++ tests/Elasticsearch/Query/Bulk/ElasticsearchBulkQueryTest.php
@@ -723,12 +723,8 @@ class ElasticsearchBulkQueryTest extends IndexTestCase
             $this->fail('Expected exception to be thrown');
         } catch (BulkWriteException $e) {
             $id = $e->errors()[0]['_id'];
-            $this->assertEquals(<<<MSG
-Error during execution of bulk write query : 
-- failed to parse field [enabled] of type [boolean] in document with id '{$id}'. Preview of field's value: 'invalid' Caused by: Failed to parse value [invalid] as only [true] or [false] are allowed.
-
-MSG
-                , $e->getMessage());
+            $this->assertStringContainsString('Error during execution of bulk write query :', $e->getMessage());
+            $this->assertStringContainsString("failed to parse field [enabled] of type [boolean] in document with id '{$id}'. Preview of field's value: 'invalid' Caused by: Failed to parse value [invalid] as only [true] or [false] are allowed.", $e->getMessage());
         }
     }
 
